1ac7d2d8ba51b2e3534195e987593ecca75d5f3d
maye <zcmaye@gmail.com>
2021-06-10 23:00:29 +0800

    add mainwindows/menus

---
+++ 5.14.2/widgets/mainwindows/menus/mainwindow.cpp
@@ -0,0 +1,31 @@
+﻿#include "mainwindow.h"
+#include<QVBoxLayout>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
+    QWidget *widget = new QWidget;
+    setCentralWidget(widget);
+
+    infoLabel = new QLabel("<i>Choose a menu option, or right-click to "
+                           "invoke a context menu</i>");
+    infoLabel->setFrameStyle(QFrame::Shape::StyledPanel | QFrame::Sunken);
+    infoLabel->setAlignment(Qt::AlignCenter);
+
+    QVBoxLayout *layout = new QVBoxLayout;
+    layout->setMargin(0);   //设置布局边距自己的边距(布局与窗口)
+    layout->setContentsMargins(5,5,5,5);//设置布局四个方向的边距(布局与元素)
+
+    layout->addStretch();
+    layout->addWidget(infoLabel);
+    layout->addStretch();
+    widget->setLayout(layout);
+
+
+}
+
+MainWindow::~MainWindow()
+{
+
+}
+
---
+++ 5.14.2/widgets/mainwindows/menus/mainwindow.h
@@ -0,0 +1,21 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+#include<QAction>
+#include<QAction>
+#include<QLabel>
+#include<QMenu>
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+private:
+    QLabel *infoLabel;  //中间信息显示
+};
+#endif // MAINWINDOW_H
